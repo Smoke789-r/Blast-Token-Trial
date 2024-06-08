a6fc845fff3429432bccd40c447c6b67cca2e41f
Smoke789-r <thunderclapper3@gmail.com>
2024-06-07 17:18:43 -0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,101 @@
+const CONTRACT_ADDRESS = '0x380Cc13f7Bbe692A4F95DEA59091b810e7C37B64';
+const ABI = [
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "owner",
+        "outputs": [
+            {
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": false,
+        "inputs": [],
+        "name": "toggleClaim",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "isClaimEnabled",
+        "outputs": [
+            {
+                "name": "",
+                "type": "bool"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "amount",
+                "type": "uint256"
+            }
+        ],
+        "name": "claim",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": true,
+                "name": "user",
+                "type": "address"
+            },
+            {
+                "indexed": false,
+                "name": "amount",
+                "type": "uint256"
+            }
+        ],
+        "name": "Claimed",
+        "type": "event"
+    }
+];
+
+window.addEventListener('load', async () => {
+    if (window.ethereum) {
+        window.web3 = new Web3(window.ethereum);
+        try {
+            await window.ethereum.enable();
+        } catch (error) {
+            console.error('User denied account access');
+        }
+    } else if (window.web3) {
+        window.web3 = new Web3(window.web3.currentProvider);
+    } else {
+        console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
+    }
+});
+
+async function claimTokens() {
+    const amount = document.getElementById('amount').value;
+    if (!amount) {
+        alert('Please enter an amount');
+        return;
+    }
+
+    const accounts = await web3.eth.getAccounts();
+    const account = accounts[0];
+
+    const contract = new web3.eth.Contract(ABI, CONTRACT_ADDRESS);
+
+    try {
+        await contract.meth
