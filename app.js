1cbe9e30dda7ed93bd61a06ca5426b43db6b71a1
Smoke789-r <thunderclapper3@gmail.com>
2024-06-07 17:54:49 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,37 @@
+const BOGE_TOKEN_ADDRESS = '0x72a06F32B46DCc9B8B64Bed1f299E3c92A83309b';
+const CLAIM_CONTRACT_ADDRESS = '0x380Cc13f7Bbe692A4F95DEA59091b810e7C37B64';
 let userAccount;
+
+const BOGE_TOKEN_ABI = [
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "_spender",
+                "type": "address"
+            },
+            {
+                "name": "_value",
+                "type": "uint256"
+            }
+        ],
+        "name": "approve",
+        "outputs": [
+            {
+                "name": "success",
+                "type": "bool"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    }
+];
 
 async function connectWallet() {
     if (window.ethereum) {
+        window.web3 = new Web3(window.ethereum);
         try {
-            // Reset the provider to ensure the prompt appears
-            window.ethereum.autoRefreshOnNetworkChange = false;
-
             // Always prompt user to select an account
             const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
             if (accounts.length > 0) {
@@ -34,4 +60,28 @@ function disconnectWallet() {
     document.getElementById('connectWalletButton').style.display = 'block';
     document.getElementById('disconnectWalletButton').style.display = 'none';
     console.log("Wallet disconnected");
+}
+
+async function approveBOGEToken() {
+    const amount = document.getElementById('amount').value;
+    if (!amount) {
+        alert('Please enter an amount');
+        return;
+    }
+
+    if (!userAccount) {
+        alert('No accounts found. Please connect Rabby Wallet.');
+        return;
+    }
+
+    const bogeTokenContract = new web3.eth.Contract(BOGE_TOKEN_ABI, BOGE_TOKEN_ADDRESS);
+
+    try {
+        // Directly use the input amount without conversion
+        await bogeTokenContract.methods.approve(CLAIM_CONTRACT_ADDRESS, amount).send({ from: userAccount });
+        alert('BOGE token approval successful!');
+    } catch (error) {
+        console.error('BOGE token approval failed', error);
+        alert('BOGE token approval failed. See console for details.');
+    }
 }
